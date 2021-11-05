78eeb1bd6a62cd07a8bac09a10139113f576a8ca
Aynur Ishbuldin <iiinjoy@gmail.com>
2021-11-06 00:25:05 +0500

    role-playing-game

---
+++ role-playing-game/src/lib.rs
@@ -0,0 +1,37 @@
+pub struct Player {
+    pub health: u32,
+    pub mana: Option<u32>,
+    pub level: u32,
+}
+
+impl Player {
+    pub fn revive(&self) -> Option<Player> {
+        if self.health == 0 {
+            Some(Player {
+                health: 100,
+                mana: if self.level >= 10 { Some(100) } else { None },
+                level: self.level,
+            })
+        } else {
+            None
+        }
+    }
+
+    pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
+        if let Some(mana) = self.mana {
+            if mana_cost > mana {
+                0
+            } else {
+                self.mana = Some(mana - mana_cost);
+                mana_cost * 2
+            }
+        } else {
+            self.health = if self.health < mana_cost {
+                0
+            } else {
+                self.health - mana_cost
+            };
+            0
+        }
+    }
+}
