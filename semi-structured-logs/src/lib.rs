98f895428bea383cceda2fe7de02b6fc9b9ec26e
Aynur Ishbuldin <iiinjoy@gmail.com>
2021-11-05 23:40:15 +0500

    semi-structured-logs

---
+++ semi-structured-logs/src/lib.rs
@@ -0,0 +1,36 @@
+/// various log levels
+#[derive(Clone, PartialEq, Debug)]
+pub enum LogLevel {
+    Info,
+    Warning,
+    Error,
+    #[cfg(feature = "add-a-variant")]
+    Debug,
+}
+/// primary function for emitting logs
+pub fn log(level: LogLevel, message: &str) -> String {
+    match level {
+        LogLevel::Info => info(message),
+        LogLevel::Warning => warn(message),
+        LogLevel::Error => error(message),
+        #[cfg(feature = "add-a-variant")]
+        LogLevel::Debug => debug(message),
+    }
+}
+
+pub fn info(message: &str) -> String {
+    format!("[INFO]: {}", message)
+}
+
+pub fn warn(message: &str) -> String {
+    format!("[WARNING]: {}", message)
+}
+
+pub fn error(message: &str) -> String {
+    format!("[ERROR]: {}", message)
+}
+
+#[cfg(feature = "add-a-variant")]
+pub fn debug(message: &str) -> String {
+    format!("[DEBUG]: {}", message)
+}
